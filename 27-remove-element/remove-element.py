5dd40f21deb045daf18bac13a5c36e33c5cc5742
Sammam Mahdi <159187442+SammamMahdi@users.noreply.github.com>
2024-03-17 22:11:50 +0600

    Time: 38 ms (51.42%) | Memory: 16.6 MB (52.29%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        c = 0
+        while c<len(nums):
+            if nums[c]==val:
+                nums.pop(c)
+            else:
+                c+=1
+            
+
+        
