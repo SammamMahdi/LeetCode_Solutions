52a5cdf83bd8c6da92b14fda0fc34c4c3de62a03
Sammam Mahdi <159187442+SammamMahdi@users.noreply.github.com>
2024-03-17 20:33:53 +0600

    Time: 616 ms (66.03%) | Memory: 22.1 MB (69.79%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        count = 0
+        max_len = 0
+        table = {0: -1}
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                count -= 1
+            else:
+                count += 1
+            if count in table:
+                max_len = max(max_len, i - table[count])
+            else:
+                table[count] = i
+        return max_len
+        
